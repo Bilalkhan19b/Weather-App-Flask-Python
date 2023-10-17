135916f9b918ed2be0b073215db0dc694d8246e8
Muhammad Bilal Khan-MBK <kahnbilal2011@gmail.com>
2023-10-17 22:53:43 +0500

    Added two modules

--- weather.py
+++ weather.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 
 def get_current_weather(city="Karachi"):
-    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
 
     weather_data = requests.get(request_url).json()
 
@@ -18,6 +18,11 @@ if __name__ == "__main__":
     print('\n*** Get Current Weather Conditions ***\n')
 
     city = input("\nPlease enter a city name: ")
+
+    # check for empty strings or string with only spaces
+    if not bool(city.strip()):
+        city = "Karachi"
+
 
     weather_data = get_current_weather(city)
 
